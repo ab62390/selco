9b6283024acca0cb66814ab405b1a3a6a858074c
ab62390 <ab0962390222@gmail.com>
2021-10-14 21:28:54 +0200

    deploy

---
+++ precache-manifest.2fc75fc2b59d4a1913ae863914bb9260.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7afd919e9a70427aeaba",
+    "url": "/selco/css/app.4ffc40b8.css"
+  },
+  {
+    "revision": "c06360ac141570fe82a4",
+    "url": "/selco/css/chunk-vendors.340d2dcb.css"
+  },
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "/selco/fonts/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "174c02fc4609e8fc4389f5d21f16a296",
+    "url": "/selco/fonts/materialdesignicons-webfont.174c02fc.ttf"
+  },
+  {
+    "revision": "64d4cf64afd77a4ad2713f648eb920e6",
+    "url": "/selco/fonts/materialdesignicons-webfont.64d4cf64.eot"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "/selco/fonts/materialdesignicons-webfont.7a44ea19.woff2"
+  },
+  {
+    "revision": "bcc9ade4dc0a9242fb13345bc293889d",
+    "url": "/selco/img/bg-01.bcc9ade4.jpg"
+  },
+  {
+    "revision": "8cfcd3e21f097da401947b0c0e0f0b7e",
+    "url": "/selco/index.html"
+  },
+  {
+    "revision": "7afd919e9a70427aeaba",
+    "url": "/selco/js/app.1d041a39.js"
+  },
+  {
+    "revision": "c06360ac141570fe82a4",
+    "url": "/selco/js/chunk-vendors.7b17faf1.js"
+  },
+  {
+    "revision": "f04d3772c4764ec13b594e724d85b4eb",
+    "url": "/selco/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/selco/robots.txt"
+  }
+]);
